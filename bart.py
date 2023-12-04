c73e68072d4b02bce91c6c1b0ccadbaa6b8da70c
Allen Zheng <allenzheng2001@github.com>
2023-12-03 20:17:54 -0600

    bart and data laod

---
+++ bart.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+from transformers import BartForSequenceClassification, BartTokenizer, AdamW
+from tqdm import tqdm as tqdm
+
+from load_datasets import test_loader, train_loader, val_loader, all_loader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+# Replace 'facebook/bart-large-cnn' with the correct model name or path
+model_name = 'facebook/bart-large-cnn'
+
+tokenizer = BartTokenizer.from_pretrained(model_name)
+model = BartForSequenceClassification.from_pretrained(model_name, num_labels=8)
+model.to(device)
+# this is a per-dimension binary classification.
+# things tried ... multiclass?
+
+def train_model():
+    optimizer = torch.optim.AdamW(model.parameters(), lr=5e-5)
+    criterion = nn.BCEWithLogitsLoss()
+    num_epochs = 5
+
+    for epoch in tqdm(range(num_epochs)):
+        for ex in tqdm(train_loader):
+            input = ex["text"]
+            input_tokens = (tokenizer(input, return_tensors='pt', truncation=True, padding=True)).to_device()
+            label = (ex["label"]).to(device)
+
+            optimizer.zero_grad()
+            output = model(**input_tokens)
+            loss = criterion(output.logits, label.float())
+            loss.backward()
+
+            optimizer.step()
+
+train_model()
+        
+
---
+++ load_datasets.py
@@ -0,0 +1,53 @@
+import torch
+import pandas as pd
+import numpy as np
+import ast
+from torch.utils.data import Dataset, DataLoader
+
+class CustomDataset(Dataset):
+    def __init__(self, texts, labels):
+        """
+        Args:
+            texts (list): List of text samples.
+            labels (list): List of binary vectors representing labels for each text sample.
+        """
+        self.texts = texts
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        label = torch.tensor(self.labels[idx], dtype=torch.float32)
+
+        return {"text": text, "label": label}
+
+def convert_evector(emotions_str_list):
+    emotions_list = ast.literal_eval(emotions_str_list)
+    emotions_vector = np.zeros(8)
+    for emotion in emotions_list:
+        emotions_vector[emotion] = 1
+    return emotions_vector
+
+def convert_ivector(intensities_str_list):
+    intensities_list = ast.literal_eval(intensities_str_list)
+    intensities_vector = np.zeros((8,3))
+    for emotion, intensity in intensities_list:
+        intensities_vector[emotion, intensity] = 1
+    return intensities_vector.ravel() # 1d vector now
+
+# Assuming you have train_texts and train_labels
+def get_data_loader(path, batch_size = 1, label = 'emotions'):
+    df = pd.read_csv(path)
+    if(label == 'intensities'):
+        dataset = CustomDataset(df['post'], [convert_ivector(gold_intensity_str) for gold_intensity_str in df['gold_intensities_ids']])
+    else:
+        dataset = CustomDataset(df['post'], [convert_evector(gold_emotion_str) for gold_emotion_str in df['gold_emotions_ids']])
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return loader
+
+train_loader = get_data_loader('CovidET_emotions/CovidET-ALL-train_val_test/train.csv')
+val_loader = get_data_loader('CovidET_emotions/CovidET-ALL-train_val_test/val.csv')
+test_loader = get_data_loader('CovidET_emotions/CovidET-ALL-train_val_test/test.csv')
+all_loader = get_data_loader('CovidET_emotions/CovidET-ALL.csv')
